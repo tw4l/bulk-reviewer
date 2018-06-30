92b38614a3113b9b358c02a6949cdf5b90620a28
Tim Walsh <timothyryanwalsh@gmail.com>
2018-06-29 23:17:56 -0400

    Add default API pagination of 100 per page

--- server/server/settings.py
+++ server/server/settings.py
@@ -52,7 +52,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 MIDDLEWARE = [
