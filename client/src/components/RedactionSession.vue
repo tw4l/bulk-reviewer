425c64b1a8dcebbf5bb7744745f56b51ec8561cb
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-02 16:42:28 -0400

    Parse file JSON into node tree and store as data

--- client/src/components/RedactionSession.vue
+++ client/src/components/RedactionSession.vue
@@ -1,5 +1,12 @@
 <template>
-<div>File tree here</div>
+  <div class="columns">
+    <div class="column">
+      File tree here
+    </div>
+    <div class="column">
+      Redaction review and actions here
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +17,7 @@ export default {
   data () {
     return {
       files: [],
+      fileTree: {},
       features: [],
       errors: []
     }
@@ -20,6 +28,7 @@ export default {
     axios.get(`http://127.0.0.1:8000/api/session/${uuid}/files/`)
       .then(response => {
         this.files = response.data.results
+        this.fileTree = this.convertPathsToTree(this.files)
       })
       .catch(e => {
         this.errors.push(e)
@@ -31,6 +40,41 @@ export default {
       .catch(e => {
         this.errors.push(e)
       })
+  },
+
+  methods: {
+    convertPathsToTree: function (files) {
+      // Build the node structure
+      const rootNode = {name: 'root', children: []}
+
+      for (let file of files) {
+        let path = file['filepath']
+        let uuid = file['uuid']
+
+        this.buildNodeRecursive(rootNode, path.split('/'), 0, uuid)
+      }
+      return rootNode
+    },
+
+    buildNodeRecursive: function (node, path, index, uuid) {
+      if (index < path.length) {
+        let item = path[index]
+        let dir = node.children.find(child => child.name === item)
+        if (!dir) {
+          dir = {
+            name: item,
+            isDir: true,
+            children: []
+          }
+          if (index === path.length - 1) {
+            dir['uuid'] = uuid
+            dir['isDir'] = false
+          }
+          node.children.push(dir)
+        }
+        this.buildNodeRecursive(dir, path, index + 1, uuid)
+      }
+    }
   }
 }
 </script>
