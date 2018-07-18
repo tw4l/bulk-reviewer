616df94325a7bd51333028fdfd3c13c61eb765cd
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-17 20:14:09 -0400

    Add option to mark files as reviewed from bulk table view

--- client/src/components/BulkViewTableRow.vue
+++ client/src/components/BulkViewTableRow.vue
@@ -2,19 +2,34 @@
   <tr>
     <td>{{ fileInfo.filepath }}</td>
     <td>{{ fileInfo.count }}</td>
-    <td><button class="button" @click="viewFile">View file</button></td>
+    <td>
+      <button class="button" @click="viewFile">View file</button>
+      <button class="button is-success" @click="markReviewed"><font-awesome-icon icon="eye-slash" class="fa-fw"></font-awesome-icon></button>
+    </td>
   </tr>
 </template>
 
 <script>
+import axios from 'axios'
 import bus from '../bus'
 
 export default {
   name: 'bulk-view-table-row',
   props: [ 'fileInfo' ],
   methods: {
-    viewFile: function (newUUID) {
+    viewFile: function () {
       bus.$emit('viewFileFromBulkTable', this.fileInfo.file_uuid)
+    },
+    markReviewed: function () {
+      let fileUUID = this.fileInfo.file_uuid
+      axios.patch(`http://127.0.0.1:8000/api/file/${fileUUID}/`, { 'cleared': true, 'redact_file': false }, { headers: { 'Content-Type': 'application/json' } })
+        .then(response => {
+          console.log(response)
+          this.alertMessage = 'Success'
+        })
+        .catch(e => {
+          console.log(e)
+        })
     }
   }
 }
--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,11 @@ import App from './App'
 import router from './router'
 import 'bulma/css/bulma.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCaretDown, faCaretRight, faFolder, faFolderOpen, faFile, faTrashAlt, faSpinner, faLevelUpAlt, faBars, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCaretRight, faFolder, faFolderOpen, faFile, faTrashAlt, faSpinner, faLevelUpAlt, faBars, faCheck, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // register fontawesome icons
-library.add(faCaretDown, faCaretRight, faFolder, faFolderOpen, faFile, faTrashAlt, faSpinner, faLevelUpAlt, faBars, faCheck)
+library.add(faCaretDown, faCaretRight, faFolder, faFolderOpen, faFile, faTrashAlt, faSpinner, faLevelUpAlt, faBars, faCheck, faEyeSlash)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
