a0ef2836ef8a64bd1c17f3fc7becc7503b3a686a
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-18 23:46:53 -0400

    Change session ul to table

--- client/src/components/Sessions.vue
+++ client/src/components/Sessions.vue
@@ -1,42 +1,43 @@
 <template>
-  <div>
-    <section class="section">
-      <new-session-modal
-        v-show="showNewSessionModal"
-        @newSessionClose="showNewSessionModal = false"></new-session-modal>
-      <div class="container">
-        <h3 class="title is-3">Sessions</h3>
-        <button class="button is-link" @click="showNewSessionModal = true">+ New Session</button>
-        <br><br>
-        <ul>
-          <li v-for="session in sessions" :key="session.uuid">
-            <div style="padding-bottom: 10px">
-              <div class="columns">
-                <div class="column">
-                  <h5 class="title is-5">{{ session.name }}</h5>
-                  <p class="subtitle is-6">{{ session.uuid }}</p>
-                </div>
-                <div class="column">
-                  <router-link
-                    :to="{ name: 'RedactionSession', params: { uuid: session.uuid }}"
-                    v-if="session.processing_complete">
-                    <button class="button is-info">Go to session</button>
-                  </router-link>
-                  <button class="button is-info" disabled v-else>
-                    <font-awesome-icon icon="spinner" class="fa-spin"></font-awesome-icon>
-                  </button>
-                  <delete-modal-button
-                    :sessionUUID="session.uuid"
-                    :sessionName="session.name">
-                  </delete-modal-button>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </section>
-  </div>
+  <section class="section">
+    <new-session-modal
+      v-show="showNewSessionModal"
+      @newSessionClose="showNewSessionModal = false"></new-session-modal>
+    <div class="container">
+      <h3 class="title is-3">Sessions</h3>
+      <button class="button is-link" @click="showNewSessionModal = true">+ New Session</button>
+      <br><br>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>UUID</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="session in sessions" :key="session.uuid">
+            <td><strong>{{ session.name }}</strong></td>
+            <td>{{ session.uuid }}</td>
+            <td>
+              <router-link
+                :to="{ name: 'RedactionSession', params: { uuid: session.uuid }}"
+                v-if="session.processing_complete">
+                <button class="button is-info">Go to session</button>
+              </router-link>
+              <button class="button is-info" disabled v-else>
+                <font-awesome-icon icon="spinner" class="fa-spin"></font-awesome-icon>
+              </button>
+              <delete-modal-button
+                :sessionUUID="session.uuid"
+                :sessionName="session.name">
+              </delete-modal-button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script>
