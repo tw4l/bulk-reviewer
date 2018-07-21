4aa21b46ce1cafd1857686e16f56b77ded568a7c
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-21 15:41:51 -0400

    Remove unnecessary import

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
