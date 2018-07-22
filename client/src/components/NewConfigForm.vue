934cff0d2fcdc3a5d152df6d9adaebcc96f27cbd
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-22 19:37:19 -0400

    Working new config form minus regex file support

--- client/src/components/NewConfigForm.vue
+++ client/src/components/NewConfigForm.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="form">
+  <form id="new-config-form" @submit.prevent="processForm">
     <!-- Name -->
     <div class="field">
       <label class="label">Name</label>
       <div class="control">
-        <input class="input" type="text" name="name" placeholder="Name">
+        <input class="input" type="text" name="name" v-model="name" placeholder="Name">
       </div>
     </div>
     <!-- Regex file upload -->
@@ -31,19 +31,19 @@
       <label class="label">Scanners</label>
       <div class="control">
         <label class="checkbox">
-          <input type="checkbox" name="pii" default="true">
+          <input type="checkbox" name="pii" default="true" v-model="pii">
           PII (SSN, credit cards, phone, email)
         </label>
       </div>
       <div class="control">
         <label class="checkbox">
-          <input type="checkbox" name="web">
+          <input type="checkbox" name="web" v-model="web">
           Web resources (URLs, domains, email/HTTP headers, HTTP logs)
         </label>
       </div>
       <div class="control">
         <label class="checkbox">
-          <input type="checkbox" name="exif">
+          <input type="checkbox" name="exif" v-model="exif">
           EXIF and GPS metadata
         </label>
       </div>
@@ -53,10 +53,10 @@
       <div class="control">
         <label class="label">SSN matching mode</label>
         <div class="select">
-          <select>
-            <option>Default (no label required, dashes required)</option>
-            <option>Strict (number must be prefaced with "SSN:", dashes required)</option>
-            <option>Inclusive (no label required, no dashes required)</option>
+          <select name="ssn" v-model="ssnMode">
+            <option value="0">Strict ("SSN" label required, dashes required)</option>
+            <option value="1">Default (dashes only required)</option>
+            <option value="2">Permissive (no label or dashes required)</option>
           </select>
       </div>
     </div>
@@ -64,18 +64,59 @@
     <!-- Buttons -->
     <div class="field is-grouped">
       <div class="control">
-        <button class="button is-link">Submit</button>
+        <button class="button is-link" :disabled="formSubmit">Submit</button>
       </div>
       <div class="control">
         <button class="button is-text">Cancel</button>
       </div>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'new-config-form'
+  name: 'new-config-form',
+  data () {
+    return {
+      formSubmit: false,
+      name: '',
+      regexFile: '',
+      pii: true,
+      web: false,
+      exif: false,
+      ssnMode: 1
+    }
+  },
+  mounted () {
+    // form event listener
+    const newConfigForm = document.getElementById('new-config-form')
+    newConfigForm.addEventListener('submit', this.processNewConfigForm)
+  },
+  methods: {
+    processForm: function (e) {
+      // disable submit button
+      this.formSubmit = true
+      // POST form
+      axios.post(`http://127.0.0.1:8000/api/config/add/`, {
+        name: this.name,
+        // regex_file
+        ssn_mode: parseInt(this.ssnMode),
+        pii_scanners: this.pii,
+        web_scanners: this.web,
+        exif_gps_scanners: this.exif
+      })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+      // re-enable submit button
+      this.formSubmit = false
+    }
+  }
 }
 </script>
 
