b051e4460c53e428cf5def2d1ec9c66a519dbadf
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-23 16:27:29 -0400

    Display Unicode chars instead of hex values

--- client/src/components/IndividualViewTableRow.vue
+++ client/src/components/IndividualViewTableRow.vue
@@ -6,14 +6,14 @@
         class="button is-small"
         v-clipboard:copy="featureInfo.feature">Copy</button>
     </td>
-    <td v-else>{{ featureInfo.feature }}
+    <td v-else>{{ unescapedFeature }}
       <button
         class="button is-small"
         v-clipboard:copy="featureInfo.feature">Copy</button>
     </td>
     <!-- Context -->
     <td v-if="showFileBrowser">{{ contextWithLineBreaks }}</td>
-    <td v-else>{{ featureInfo.context }}</td>
+    <td v-else>{{ unescapedContext }}</td>
     <!-- Buttons -->
     <td v-if="featureInfo.cleared === false">
       <button class="button is-info" @click="markCleared"><font-awesome-icon icon="eye-slash"></font-awesome-icon></button>
@@ -57,13 +57,19 @@ export default {
     }
   },
   computed: {
+    unescapedFeature: function () {
+      return this.featureInfo.feature.replace(/\\x[a-fA-F0-9]{2}/g, String.fromCharCode('$1'))
+    },
+    unescapedContext: function () {
+      return this.featureInfo.context.replace(/\\x[a-fA-F0-9]{2}/g, String.fromCharCode('$1'))
+    },
     featureWithLineBreaks: function () {
       // Add a space tag every 40 chars for narrow display
-      return this.featureInfo.feature.replace(/(.{40})/g, '$1 ')
+      return this.funescapedFeature.replace(/(.{40})/g, '$1 ')
     },
     contextWithLineBreaks: function () {
       // Add a space tag every 40 chars for narrow display
-      return this.featureInfo.context.replace(/(.{40})/g, '$1 ')
+      return this.unescapedContext.replace(/(.{40})/g, '$1 ')
     }
   }
 }
