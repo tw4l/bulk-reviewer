b7bc0ca77326e8b7b2055f329268160645c7026d
Tim Walsh <timothyryanwalsh@gmail.com>
2018-07-24 21:45:43 -0400

    Show file browser by default

--- client/src/components/RedactionSession.vue
+++ client/src/components/RedactionSession.vue
@@ -83,7 +83,7 @@ export default {
       errors: [],
       currentlySelectedUUID: '',
       redactionView: false,
-      showFileBrowser: false
+      showFileBrowser: true
     }
   },
   created () {
