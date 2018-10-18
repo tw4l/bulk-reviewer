e43ee62acc35f505c5b28b864a24d31f9950800d
Tim Walsh <timothyryanwalsh@gmail.com>
2018-10-18 14:58:48 -0400

    Upgrade to Vue CLI 3

--- client/src/main.js
+++ client/src/main.js
@@ -20,10 +20,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
