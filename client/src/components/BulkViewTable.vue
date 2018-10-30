51a8afe1e222f5b45a0f3aaef7a727161997a34d
Tim Walsh <timothyryanwalsh@gmail.com>
2018-10-30 18:52:12 -0400

    Add basic pagination to feature tables

--- client/src/components/BulkViewTable.vue
+++ client/src/components/BulkViewTable.vue
@@ -1,23 +1,31 @@
 <template>
-  <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-    <thead>
-      <tr>
-        <th>File</th>
-        <th>Count</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <bulk-view-table-row
-        v-for="f in sortedFileData"
-        :key="f.uuid"
-        :fileInfo="f"
-        :features="features"
-        :featureType="featureType"
-        :showFileBrowser="showFileBrowser">
-      </bulk-view-table-row>
-    </tbody>
-  </table>
+  <div>
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>File</th>
+          <th>Count</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <bulk-view-table-row
+          v-for="f in paginatedFileData"
+          :key="f.uuid"
+          :fileInfo="f"
+          :features="features"
+          :featureType="featureType"
+          :showFileBrowser="showFileBrowser">
+        </bulk-view-table-row>
+      </tbody>
+    </table>
+    <!-- Pagination -->
+    <div class="buttons has-addons">
+      <button class="button" @click="decrementStartIndex" :disabled="firstPage">Previous</button>
+      <button class="button" disabled>{{ currentPage }} of {{ numberOfPages }}</button>
+      <button class="button" @click="incrementStartIndex" :disabled="lastPage">Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,6 +35,12 @@ export default {
   name: 'bulk-view-table',
   components: { BulkViewTableRow },
   props: [ 'fileData', 'features', 'featureType', 'showFileBrowser' ],
+  data () {
+    return {
+      startIndex: 0,
+      pageCount: 5
+    }
+  },
   methods: {
     compare: function (a, b) {
       // ensure that counts are ints before sorting
@@ -40,12 +54,39 @@ export default {
         comparison = -1
       }
       return comparison
+    },
+    resetStartIndex () {
+      this.startIndex = 0
+    },
+    incrementStartIndex () {
+      this.startIndex += this.pageCount
+    },
+    decrementStartIndex () {
+      this.startIndex -= this.pageCount
     }
   },
   computed: {
     sortedFileData: function () {
       let arrayToChange = this.fileData
       return arrayToChange.sort(this.compare)
+    },
+    paginatedFileData () {
+      return this.sortedFileData.slice(this.startIndex, this.startIndex + this.pageCount)
+    },
+    numberOfPages () {
+      return Math.ceil(this.sortedFileData.length / this.pageCount)
+    },
+    currentPage () {
+      return this.startIndex === 0 ? 1 : Math.ceil(this.startIndex / this.pageCount) + 1
+    },
+    moreThanOnePage () {
+      return this.numberOfPages > 1
+    },
+    firstPage () {
+      return this.currentPage === 1
+    },
+    lastPage () {
+      return this.currentPage >= this.numberOfPages
     }
   }
 }
