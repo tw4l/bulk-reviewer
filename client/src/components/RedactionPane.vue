a4bfeeb233b083c6ad1c036c69dedb86476e708e
Tim Walsh <timothyryanwalsh@gmail.com>
2018-11-28 16:40:55 -0500

    Change labels for total, confirmed, dismissed counts

--- client/src/components/RedactionPane.vue
+++ client/src/components/RedactionPane.vue
@@ -45,9 +45,9 @@
     </div>
     <!-- Metadata -->
     <div style="margin-bottom: 15px;">
-      <p><strong>Total:</strong> {{ featureCount }}</p>
+      <p><strong>Total results:</strong> {{ featureCount }}</p>
+      <p><strong>Confirmed sensitive:</strong> {{ featuresNotClearedCount }}</p>
       <p><strong>Dismissed:</strong> {{ featuresClearedCount }} <span v-if="featuresClearedCount !== 0"><button class="button is-small" @click="toggleShowDismissed" v-tooltip="'Review results dismissed as false positives'">View</button></span></p>
-      <p><strong>Remaining:</strong> {{ featuresNotClearedCount }}</p>
       <div class="buttons">
         <button class="button" @click="unclearAll" v-if="(featuresClearedCount > 0) && (viewingFile === false)" v-tooltip="'Reset session to original state'">Reset</button>
         <button class="button is-info" @click="unclearAll" v-else-if="(featuresClearedCount > 0)" v-tooltip="'Confirm all results as sensitive'">Confirm all</button>
